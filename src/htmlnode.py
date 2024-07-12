07be1657ae0eefa968e51afffa0dbb3b73439632
Xavier-Hsiao <tim88776@gmail.com>
2024-07-12 22:49:42 +0800

    add leafnode class and its unittest

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,7 +5,7 @@ class HTMLNode():
 		self.value = value
 		self.children = children
 		self.props = props
-	def to_HTML(self):
+	def to_html(self):
 		raise NotImplementedError("Must be overridden by derived classes")
 	def props_to_html(self):
 		# original: {"href": "https://www.google.com", "target": "_blank"}
@@ -18,4 +18,14 @@ class HTMLNode():
 		return props_html
 	def __repr__(self):
 		return f"HTMLNode(tag:{self.tag}, value:{self.value}, children:{self.children}, props: {self.props})"
-		
+
+class LeafNode(HTMLNode):
+	def __init__(self, tag, value, props=None):
+		super().__init__(tag, value, None, props)
+	def to_html(self):
+		if self.value is None:
+			raise ValueError("Leafnode requires a value")
+		if self.tag is None:
+			return str(self.value)
+		props_html = self.props_to_html()
+		return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 	def test_to_html_props(self):
@@ -13,6 +13,14 @@ class TestHTMLNode(unittest.TestCase):
 			node.props_to_html(),
 			' href="https://cpbl.com.tw" class="baseball"',
 		)
+
+	def test_to_html_no_children(self):
+		node = LeafNode("p", "My cat is adorable")
+		self.assertEqual(node.to_html(), "<p>My cat is adorable</p>")
+
+	def test_to_html_no_tag(self):
+		node = LeafNode(None, "I don't have any tag")
+		self.assertEqual(node.to_html(), "I don't have any tag")
 
 if __name__ == "__main__":
 	unittest.main()
