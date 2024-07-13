9b73f3d98e41132d8df4ccb8a09127ffdb2b760d
Xavier-Hsiao <tim88776@gmail.com>
2024-07-13 10:28:24 +0800

    add ParentNode subclass and its unittest

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,6 @@
+from ast import If
+
+
 class HTMLNode():
 	"""docstring for HTMLNode"""
 	def __init__(self, tag=None, value=None, children=None, props=None):
@@ -5,8 +8,10 @@ class HTMLNode():
 		self.value = value
 		self.children = children
 		self.props = props
+
 	def to_html(self):
 		raise NotImplementedError("Must be overridden by derived classes")
+	
 	def props_to_html(self):
 		# original: {"href": "https://www.google.com", "target": "_blank"}
 		# Return:  href="https://www.google.com" target="_blank"
@@ -16,12 +21,14 @@ class HTMLNode():
 		for key, value in self.props.items():
 		 	props_html += f' {key}="{value}"'
 		return props_html
+	
 	def __repr__(self):
 		return f"HTMLNode(tag:{self.tag}, value:{self.value}, children:{self.children}, props: {self.props})"
 
 class LeafNode(HTMLNode):
 	def __init__(self, tag, value, props=None):
 		super().__init__(tag, value, None, props)
+
 	def to_html(self):
 		if self.value is None:
 			raise ValueError("Leafnode requires a value")
@@ -29,3 +36,20 @@ class LeafNode(HTMLNode):
 			return str(self.value)
 		props_html = self.props_to_html()
 		return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+	def __init__(self, tag, children, props=None):
+		super().__init__(tag, None, children, props)
+	
+	def to_html(self):
+		if self.tag is None:
+			raise ValueError("ParentNode requires a tag")
+		if self.children is None:
+			raise ValueError("ParentNode must have children")
+		
+		# Recursion being called on a nested child node
+		props_html = self.props_to_html()
+		children_html = ""
+		for child in self.children:
+			children_html += child.to_html()
+		return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
+from lib2to3.pytree import Leaf
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 	def test_to_html_props(self):
@@ -21,6 +22,29 @@ class TestHTMLNode(unittest.TestCase):
 	def test_to_html_no_tag(self):
 		node = LeafNode(None, "I don't have any tag")
 		self.assertEqual(node.to_html(), "I don't have any tag")
+
+	def test_to_html_with_children(self):
+		child_node = LeafNode("span", "Cute baby")
+		parent_node = ParentNode("p", [child_node])
+		self.assertEqual(parent_node.to_html(), "<p><span>Cute baby</span></p>")
+	
+	def test_to_html_with_grandchildren(self):
+		grandchild_node = LeafNode("b", "Young and sweet")
+		child_node = ParentNode("span", [grandchild_node])
+		parent_node = ParentNode("div", [child_node])
+		self.assertEqual(parent_node.to_html(), "<div><span><b>Young and sweet</b></span></div>")
+
+	def test_to_html_with_many_children(self):
+		test_node = ParentNode(
+			"p",
+			[
+				LeafNode("b", "Bold text"),
+				LeafNode(None, "Normal text"),
+				LeafNode("i", "Italic text"),
+				LeafNode(None, "Normal text"),
+			]
+		) 
+		self.assertEqual(test_node.to_html(), "<p><b>Bold text</b>Normal text<i>Italic text</i>Normal text</p>")
 
 if __name__ == "__main__":
 	unittest.main()
