e21ff227f9ba69206c6abfe25a1dc27506688c41
Xavier Hsiao <xavier.hsiao@0818XavierHsiao1.local>
2025-03-06 17:24:21 +0800

    feat: add function to split raw markdown text into TextNodes based on images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -34,3 +34,59 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+'''
+Input:
+node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+)
+
+Output:
+new_nodes = split_nodes_link([node])
+# [
+#     TextNode("This is text with a link ", TextType.TEXT),
+#     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+#     TextNode(" and ", TextType.TEXT),
+#     TextNode(
+#         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+#     ),
+# ]
+'''
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        # We only split text-type objects
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        # Todo: copy the text of the old_node
+        original_text = old_node.text
+        # Todo: extrat images from the old_node
+        images = extract_markdown_images(original_text)
+        # Todo: if there are no images -> return a list of the original TextNode
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            return old_nodes
+        # Todo: loop throgh the images, use image as the delimiter and split the text into two sections
+        for image in images:
+            old_node_sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            # Todo: handle invalid sytax of missing closing symbol
+            if len(old_node_sections) != 2:
+                raise ValueError("invalid markdown syntax: probably missing closing symbols")
+            # Todo: skip leading empty string 
+            if old_node_sections[0] == "":
+                continue
+            new_nodes.append(TextNode(old_node_sections[0], TextType.TEXT))
+            # Todo: append image-type node
+            new_nodes.append(TextNode(
+                image[0],
+                TextType.IMAGE,
+                image[1],
+            ))
+            # Todo: skip trailing empty string
+            original_text = old_node_sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,11 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links,
+    split_nodes_image,
+)
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -78,6 +83,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 ("to cpbl website", "https://www.cpble.com.tw"),
                 ("to youtube", "https://www.youtube.com"),
             ]
+        )
+    
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
         )
 
 if __name__ == "__main__": 
