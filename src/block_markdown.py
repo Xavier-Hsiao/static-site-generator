67e3788e0124e65ba022b88846cd5202c04e426f
Xavier <tim88776@gmail.com>
2025-03-08 16:24:13 +0800

    feat: add function to detect block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 # Arg: a raw Markdown string
 # Return: a list of block strings
 def markdown_to_blocks(markdown):
@@ -11,3 +23,33 @@ def markdown_to_blocks(markdown):
         cleaned_block = block.strip()
         new_strings.append(cleaned_block)
     return new_strings
+
+# Arg: a raw Markdown block of text
+# Return: a BlockType object
+def block_to_block_type(block):
+    # should deal with multlines block
+    lines = block.split("\n")
+
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.ORDERED_LIST
+            i += 1
+        return BlockType.ORDERED_LIST 
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    block_to_block_type, 
+    markdown_to_blocks,
+    BlockType,
+)
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -42,4 +46,57 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+    
+    def test_blcok_type_heading(self):
+        md = """
+## I am H2
+    """
+
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.HEADING
+        )
+    
+    def test_blcok_type_heading_false(self):
+        md = """
+####I am H2
+    """
+
+        self.assertNotEqual(
+            block_to_block_type(md),
+            BlockType.HEADING
+        )
+    
+    def test_blcok_type_code(self):
+        md = """
+```Python
+I am a block of code
+```
+    """
+
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.CODE
+        )
+    
+    def test_block_type_code_false(self):
+        md = """
+```Python
+I am missing the closing triple ticks...
+"""
+
+        self.assertNotEqual(
+            block_to_block_type(md),
+            BlockType.CODE
+        )
+    
+    def test_block_type_quote(self):
+        md = """
+>This is a inspirational quote!\nContinue
+"""
+
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.QUOTE
         )
